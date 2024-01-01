c7c3be594e60a52184b7223e490765fce8201080
patrickojunde-cp <141427768+patrickojunde-cp@users.noreply.github.com>
2024-01-01 10:29:02 +0100

    refactor rust module

--- algorithmswithrust/grokkingalgorithm/chapter1/src/main.rs
+++ algorithmswithrust/grokkingalgorithm/chapter1/src/main.rs
@@ -1,14 +1,14 @@
-use iteration::binary_search_with_iteration;
-use recursion::binary_search_with_recursion;
+use binarysearch::iteration::binary_search_with_iteration;
+use binarysearch::recursion::binary_search_with_recursion;
 
-pub mod iteration;
-pub mod recursion;
+
+mod binarysearch;
 
 fn main() {
 
     let mut student_records: Vec<String> = Vec::new();
     student_records.push(String::from("Patrick"));
-    student_records.push(String::from("Omolade"));
+    student_records.push(String::from("oke"));
     student_records.push(String::from("Oyin"));
     student_records.push(String::from("Funke"));
     let item = &String::from("Oyin");
